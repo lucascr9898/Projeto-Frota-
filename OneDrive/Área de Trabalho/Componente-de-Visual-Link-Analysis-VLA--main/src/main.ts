b461a1fa61e8fab9a06f99de9e1ae83f20430279
lucascr9898 <kimimiminb@hotmail.com>
2026-07-01 22:23:08 -0300

    Revert "quase finalização do componente principal"
    This reverts commit 864ff0e0d6aaed5e7bb1f34f213c235be6f282b9.
    
    :wq#

--- OneDrive/Área de Trabalho/Componente-de-Visual-Link-Analysis-VLA--main/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './via-viewer/app.config';
-import { App } from './via-viewer/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
--- OneDrive/Área de Trabalho/Componente-de-Visual-Link-Analysis-VLA--main/src/via-viewer/models/vla-interface.ts
+++
@@ -1,7 +0,0 @@
-import { VlaEdge } from "./vla-edge.model";
-import { VlaNode } from "./vla-node.model";
-
-export interface VlaInterface {
-    nodes: VlaNode[];
-    edges: VlaEdge[];
-}
